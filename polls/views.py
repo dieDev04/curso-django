07dd69a00fc7a3a5877c91c07725f96d1987af5a
Diego aroca <diego.aroca@energiasolarsa.com>
2025-03-25 17:16:19 -0500

    New views

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # from django.template import loader
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from . import models
 
 
-# Create your views here.
 def index(request):
     latest_question_list = models.Question.objects.order_by("-pub_date")[:5]
     # template = loader.get_template("polls/index.html")
@@ -14,14 +12,26 @@ def index(request):
     # }
     # return HttpResponse(template.render(context, request))
 
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = models.Question.objects.get(pk=question_id)
+    # except models.Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    
+    question = get_object_or_404(models.Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
 
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 
 def vote(requets, question_id):
